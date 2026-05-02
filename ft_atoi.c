c86bd8149e1304705bce26c0db9d1c3d722a91cd
Leticia Maria Pozo Moura <lpozo-mo@car8s3.42barcelona.com>
2026-05-02 18:58:21 +0200

    mi libft amo a santi y a mafia

---
+++ ft_atoi.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lpozo-mo <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/02 15:59:13 by lpozo-mo          #+#    #+#             */
+/*   Updated: 2026/05/02 16:13:42 by lpozo-mo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+//#include <stdio.h>
+
+int	ft_atoi(const char *str)
+{
+	int	i;
+	int	sign;
+	int	result;
+
+	i = 0;
+	sign = 1;
+	result = 0;
+	while (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' 
+		|| str[i] == '\r' || str[i] == '\f' || str[i] == '\v')
+		i++;
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if(str[i] == '-')
+			sign = -1;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		result = result * 10 + (str[i] - '0');
+		i++;
+	}
+	return (result * sign);
+}
+
+/*int	main(void)
+{
+	printf("%d\n", ft_atoi ("42"));
+	printf("%d\n", ft_atoi("-5"));
+	printf("%d\n", ft_atoi("  42"));
+	printf("%d\n", ft_atoi("+10"));
+	printf("%d\n", ft_atoi("42abc"));
+	return (0);
+}*/
---
+++ ft_calloc.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lpozo-mo <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/02 18:10:12 by lpozo-mo          #+#    #+#             */
+/*   Updated: 2026/05/02 18:33:51 by lpozo-mo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdio.h>
+
+void	*ft_calloc(size_t nmemb, size_t size)
+{
+	void	*ptr;
+
+	ptr = malloc(nmemb *size);
+	if (ptr == NULL)
+		return (NULL);
+	ft_bzero(ptr, nmemb * size);
+	return (ptr);
+}
+
+int	main(void)
+{
+	int	*nums;
+	char	*str;
+
+	nums = ft_calloc(5, sizeof(int));
+	printf("%d %d %d\n", nums[0], nums[2], nums[4]);
+	free(nums);
+	str = ft_calloc(6, sizeof(char));
+	str[0] = 'H';
+	str[1] = 'o';
+	str[2] = 'l';
+	str[3] = 'a';
+	str[4] = '!';
+	printf("%s\n", str);
+	free(str);
+	return (0);
+}
